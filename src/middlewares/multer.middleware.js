ec01d8bb9fd905ca92eff54c4f5ecd58136d0250
IfBilal <muhammadbilaltahir06@gmail.com>
2025-10-18 21:38:19 +0500

    basic setup

---
+++ src/middlewares/multer.middleware.js
@@ -0,0 +1,37 @@
+import multer from "multer";
+import crypto from "crypto";
+import path from "path";
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "./public/temp");
+  },
+  filename: function (req, file, cb) {
+    let randomName = crypto.randomBytes(12).toString("hex");
+    let extension = path.extname(file.originalname).toLowerCase();
+    cb(null, `${file.fieldname}-${Date.now()}-${randomName}${extension}`);
+  },
+});
+
+// fileFilter for images only
+const imageFileFilter = (req, file, cb) => {
+  if (file.mimetype.startsWith("image/")) {
+    cb(null, true); // accept file
+  } else {
+    cb(new Error("Only image files are allowed!"), false); // reject file
+  }
+};
+
+// fileFilter for videos only
+const videoFileFilter = (req, file, cb) => {
+  if (file.mimetype.startsWith("video/")) {
+    cb(null, true);
+  } else {
+    cb(new Error("Only video files are allowed!"), false);
+  }
+};
+
+
+// multer upload instances
+export const uploadImages = multer({ storage, fileFilter: imageFileFilter });
+export const uploadVideos = multer({ storage, fileFilter: videoFileFilter });
