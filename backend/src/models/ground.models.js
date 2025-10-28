7c5ec36e97560283fa583462d253e2ba20be6813
IfBilal <muhammadbilaltahir06@gmail.com>
2025-10-28 20:25:04 +0500

    user register and login

---
+++ backend/src/models/ground.models.js
@@ -0,0 +1,64 @@
+import mongoose, { Schema } from "mongoose";
+
+const groundSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    description: {
+      type: String,
+      trim: true,
+    },
+    city: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    coverImage: {
+      url: { type: String, required: true },
+      publicId: { type: String, required: true },
+    },
+    ownerPassword: {
+      type: String,
+      required: true,
+    },
+    sportTypes: {
+      type: [String],
+      required: true,
+      default: [],
+    },
+    location: {
+      type: String,
+      required: true,
+    },
+    basePrice: {
+      type: Number,
+      required: true,
+      min: 0,
+    },
+    availableHours: {
+      start: { type: String, required: true },
+      end: { type: String, required: true },
+      slotDuration: 60,
+    },
+    amenities: {
+      type: [String],
+      default: [],
+    },
+    photos: [
+      {
+        url: { type: String, required: true },
+        publicId: { type: String, required: true },
+      },
+    ],
+    rules: {
+      type: String,
+      trim: true,
+    },
+  },
+  { timestamps: true }
+);
+
+export const Ground = mongoose.model("Ground", groundSchema);
