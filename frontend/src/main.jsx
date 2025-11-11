a4276804626d48c43cad92f8a08e74766bd200c4
T361 <tshaukat.25@gmail.com>
2025-11-11 16:33:20 +0500

    routes protected

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,52 +1,66 @@
-import PrivateRoute from "./PrivateRoute.jsx";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-<Routes>
-  <Route path="/" element={<Login />} />
-  <Route path="/signup" element={<SignUp />} />
-  <Route path="/otp" element={<OtpVerification />} />
-  
-  {/* Protected routes */}
-  <Route
-    path="/homepage"
-    element={
-      <PrivateRoute>
-        <Homepage />
-      </PrivateRoute>
-    }
-  />
-  <Route
-    path="/groundbooking"
-    element={
-      <PrivateRoute>
-        <BookingPage />
-      </PrivateRoute>
-    }
-  />
-  <Route
-    path="/add-ground"
-    element={
-      <PrivateRoute>
-        <AddGround />
-      </PrivateRoute>
-    }
-  />
-  <Route
-    path="/change-password"
-    element={
-      <PrivateRoute>
-        <ChangePassword />
-      </PrivateRoute>
-    }
-  />
-  <Route
-    path="/admin-dashboard"
-    element={
-      <PrivateRoute>
-        <AdminPage />
-      </PrivateRoute>
-    }
-  />
+import Login from "./Login.jsx";
+import SignUp from "./SignUp/page.jsx";
+import OtpVerification from "./Otp/page.jsx";
+import Homepage from "./Homepage/page.jsx";
+import GuestHome from "./GuestHome/page.jsx";
+import BookingPage from "./GroundBooking/page.jsx";
+import AddGround from "./AddGround/page.jsx";
+import ChangePassword from "./ChangePass/page.jsx";
+import AdminLogin from "./AdminLogin/page.jsx";
+import AdminPage from "./AdminPage/page.jsx";
 
-  {/* Public route */}
-  <Route path="/guesthome" element={<GuestHome />} />
-</Routes>
+import PrivateRoute from "./PrivateRoute.jsx"; // Protected routes wrapper
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <BrowserRouter>
+      <Routes>
+        {/* Public routes */}
+        <Route path="/" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/otp" element={<OtpVerification />} />
+        <Route path="/guesthome" element={<GuestHome />} />
+        <Route path="/adminlogin" element={<AdminLogin />} />
+        <Route path="/add-ground" element={<AddGround />} /> {/* PUBLIC */}
+
+        {/* Protected routes */}
+        <Route
+          path="/homepage"
+          element={
+            <PrivateRoute>
+              <Homepage />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/groundbooking"
+          element={
+            <PrivateRoute>
+              <BookingPage />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/change-password"
+          element={
+            <PrivateRoute>
+              <ChangePassword />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/admin-dashboard"
+          element={
+            <PrivateRoute>
+              <AdminPage />
+            </PrivateRoute>
+          }
+        />
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
+);
